73d2b60240addf6be51d071e39839e19dd4ecdee
Sujith <syjithkpy@gmail.com>
2026-01-27 05:01:28 +0000

    added something

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import { system } from './theme/index.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<ChakraProvider value={system}>
+  <App />
+</ChakraProvider>
+
 )
---
+++ src/theme/index.ts
@@ -0,0 +1,17 @@
+// theme/system.ts
+import { createSystem, defineConfig } from "@chakra-ui/react";
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {
+        primary: { value: "#2563eb" },
+      },
+      radii: {
+        md: { value: "8px" },
+      },
+    },
+  },
+});
+
+export const system = createSystem(config);
